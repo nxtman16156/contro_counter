979c5a3d5297ef39aa67df12985dd5e68d6b189a
nxtman <peter.a.russell1950@gmail.com>
2018-09-27 11:39:06 +0000

    Push to Heroku

--- public/script.js
+++ public/script.js
@@ -42,11 +42,35 @@ window.onload = function() {
             $("#class2").hide();
             $("#class1").show();
         }
+        
+        $("#average_count").html(calculateAverage());
     });
     
     socket.on("up", function(data) {
         for (var i = 0; i < 20; i++) {
             $("#counter" + (i + 1)).html(data[i]);
         }
+        var average = calculateAverage();
+        $("#average_count").html(average);
+        for (var i = 1; i <= 19; i++) {
+            var value = parseInt($("#counter" + i).html());
+            if (value > average + 2 || value < average - 2) {
+                $("#counter" + i).css("background-color", "#ef6502");
+            } else {
+                $("#counter" + i).css("background-color", "#ffffff");
+            }
+        }
     });
 };
+
+function calculateAverage() {
+    var sum = 0;
+    var count = 0;
+    for (var i = 1; i <= 19; i++) {
+        if ($("#counter" + i).is(":visible")) {
+            sum += parseInt($("#counter" + i).html());
+            count++;
+        }
+    }
+    return (sum / count);
+}
