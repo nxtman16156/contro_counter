a066470ad3fa466569656b310480a963c19bf253
billmca <45943601+billmca@users.noreply.github.com>
2019-04-19 14:15:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ Names seperated with , commas
 */
 
 var class1 = ["McKen Z", "Duncan", "Run After", "Rise Up", "Rean Yoo", "Irish Buddhist", "You Don't Mess with the Mohan", "Miles"];
-var class2 = ["PopnLock", "Helene Belete", "Jimmy", "Trohia", "Ivanka", "BiTS", "Curse", "Jane", "Nguyener", "LOUD", "Bo-WO", "Derk McGerk", "Joey", "Puchi", "Tuyet"];
+var class2 = ["PopnLock", "Helene Belete", "Jimmy", "Trohia", "Ivanka", "BiTS", "Curse", "Lil' Jane", "Nguyener", "LOUD", "Bo-WO", "Derk McGerk", "Joey", "Puchi", "Tuyet"];
 
 var ouch1 = [];
 var ouch2 = [];
